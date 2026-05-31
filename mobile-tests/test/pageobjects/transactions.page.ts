ccfea8601f7963812ff228c8749a66247e74b37f
pilarsilveyra <psilveyra@mail.austral.edu.ar>
2026-05-31 16:30:03 -0300

    test(appium): add transactions history flow

--- mobile-tests/test/pageobjects/transactions.page.ts
+++ mobile-tests/test/pageobjects/transactions.page.ts
@@ -3,8 +3,21 @@ class TransactionsPage {
         return $('android=new UiSelector().text("Historial de transacciones")');
     }
 
+    transactionTicker(ticker: string) {
+        return $(`~transaction_ticker_${ticker}`);
+    }
+
+    transactionType(type: 'BUY' | 'SELL', ticker: string) {
+        return $(`~transaction_type_${type}_${ticker}`);
+    }
+
     async expectVisible() {
         await expect(this.title).toBeDisplayed();
+    }
+
+    async expectBuyTransaction(ticker: string) {
+        await expect(this.transactionTicker(ticker)).toBeDisplayed();
+        await expect(this.transactionType('BUY', ticker)).toBeDisplayed();
     }
 }
 
